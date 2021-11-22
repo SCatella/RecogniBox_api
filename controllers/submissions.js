3366216f5ea78f6d4f96043a8ea3dd3baacb8adf
SCatella <samuel.catella@gmail.com>
2021-11-22 12:25:45 -0700

    initial commit

---
+++ controllers/submissions.js
@@ -0,0 +1,32 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: 'c5d2fa0b6264485fb6200a4bc1fea31b'
+});
+
+const handleApiCall = () => (req, res) => {
+    app.models
+        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('Unable to work with API'));
+}
+
+const handleSubmissions = (db) => (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('submissions', 1)
+        .returning('submissions')
+        .then(submissions => {
+            res.json(submissions[0]);
+        })
+        .catch(err => {
+            res.status(400).json('Unable to get entries');
+        })
+}
+
+module.exports = {
+    handleSubmissions,
+    handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const signin = require('./controllers/signin');
+const register = require('./controllers/register');
+const submissions = require('./controllers/submissions');
+const profile = require('./controllers/profile');
+
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    port : 5432,
+    user : 'postgres',
+    password : 'Sc-0338374',
+    database : 'recognibox'
+  }
+});
+
+const app = express();
+
+
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Server is operational.');
+})
+
+app.post('/signin', signin.handleSignIn(db, bcrypt));
+
+app.post('/register', register.handleRegister(db, bcrypt));
+
+app.get('/profile/:id', profile.handleProfileGet(db));
+
+app.put('/submissions', submissions.handleSubmissions(db));
+app.post('/submissionsurl', submissions.handleApiCall());
+
+app.listen(3000, () => {
+    console.log('App is running on port 3000');
+})
