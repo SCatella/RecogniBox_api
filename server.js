d8fbbc12779bc3ea5542f179e74fceb9a01ee97d
SCatella <samuel.catella@gmail.com>
2021-11-22 14:20:21 -0700

    found the PORT issue

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.get('/profile/:id', profile.handleProfileGet(db));
 app.put('/submissions', submissions.handleSubmissions(db));
 app.post('/submissionsurl', submissions.handleApiCall());
 
-app.listen(3000, () => {
-    console.log('App is running on port 3000');
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`App is running on port ${ process.env.PORT }`);
 })
