d565f9904188a301a74e94b5516d28a480c3ff90
SCatella <samuel.catella@gmail.com>
2021-11-23 11:29:29 -0700

    adding another env to get db to work

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const submissions = require('./controllers/submissions');
 const profile = require('./controllers/profile');
 
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 const db = knex({
   client: 'pg',
   connection: {
